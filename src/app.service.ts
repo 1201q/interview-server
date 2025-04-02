bf929a4425ed3a38b8bf88aff048cdf3651450b4
hwang <1201q@naver.com>
2025-04-02 10:58:30 +0900

    test: 최신 변경 사항이 반영되는지 테스트.
    issue: #5

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hello World! plz plz plz plz";
+    return "Hello World! ✅✅✅✅✅✅✅✅";
   }
 }
