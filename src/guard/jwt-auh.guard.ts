a86d0b2fe4a1d5de9754fcb08418804075b1430b
hwang <1201q@naver.com>
2025-04-03 18:33:18 +0900

    feat: guard 추가
    issue: #6

---
+++ src/guard/jwt-auh.guard.ts
@@ -0,0 +1,31 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard("jwt") {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(error: any, user: any, info: any) {
+    if (error) {
+      throw new UnauthorizedException(`인증 실패: ${error.message}`);
+    }
+
+    if (!user) {
+      if (info?.message === "No auth token") {
+        throw new UnauthorizedException("인증 실패: 액세스 토큰이 필요합니다.");
+      } else if (info?.message === "jwt expired") {
+        throw new UnauthorizedException("인증 실패: 토큰이 만료되었습니다.");
+      } else {
+        throw new UnauthorizedException("인증 실패: 유효하지 않은 토큰입니다.");
+      }
+    }
+
+    return user;
+  }
+}
