22ea0fdc65496ca2e90fb9d7fa81e0791f5e96c6
hwang <1201q@naver.com>
2025-08-16 15:21:32 +0900

    chore: dotenv 버전 다운그레이드

--- src/transcribe/transcribe.controller.ts
+++ src/transcribe/transcribe.controller.ts
@@ -12,6 +12,7 @@ export class TranscribeController {
     private readonly transcribeService: TranscribeService,
   ) {}
 
+  //
   @Post("/realtime/token")
   @ApiCookieAuth("accessToken")
   @ApiOperation({ summary: "gpt-4o-transcribe 토큰 발급" })
